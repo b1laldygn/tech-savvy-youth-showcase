ba8915629fde76a40b17c38e7b55c176f1e108bd
Bilal Efe Doygun <bilaldygn@gmail.com>
2025-09-03 12:02:09 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/testing/",  // <-- GitHub Pages repo adın
+  base: "testing",  // <-- GitHub Pages repo adın
   server: {
     host: "::",
     port: 8080,
